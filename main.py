f143d5b6e5ff92735c887eb47b9322e591a10988
MohamedEZ-zaalyouy <mohamedezzaalyouy.98@gmail.com>
2023-03-05 16:27:24 +0100

    Web Scraping 1

---
+++ main.py
@@ -0,0 +1,13 @@
+# ---------------------------------------------------
+# -- Web Scraping => Control Browser With Selenium --
+# ---------------------------------------------------
+# - Control Browser With Selenium For Automated Testing
+# - Download File From The Internet
+# - Subtitle Download And Add On Your Movies [ Many Modules ]
+# - Get Quotes From Websites
+# - Get Gold and Currencies Rate
+# - Get News From Websites
+# - --------------------------------------------
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
